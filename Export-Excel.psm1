95dcbd3f15cafca22b445e6db1a5ffaa9b60f214
RickWilcox <rickwilcox@gmail.com>
2018-03-11 19:15:50 -0500

    Updated more comments and minor updates to logic. Changed the way a new sheet is created and existing sheet will now be cleared and overwritten.

--- Export-Excel.psm1
+++ Export-Excel.psm1
@@ -13,7 +13,7 @@ Function Export-Excel {
             Mandaotry PSObject that will be written to the spreadsheet with headers and rows.
         .PARAMETER  WorkbookPath
             Mandatory string that contains the path to either and existing Excel workbook or the path to which the new workbook will be saved. 
-        .PARAMETER  SheetTitle
+        .PARAMETER  SheetName
             Non-mandatory string that will become the title of the sheet that is to be created. If the sheet already exists, the sheet will created with an index number at the end.
         .PARAMETER  MaxColumnWidth
             Non-mandatory value that will create columns at the width provided.
@@ -33,7 +33,7 @@ Function Export-Excel {
         [String]$WorkbookPath,
 
         [Parameter(Mandatory = $False)]
-        [String]$SheetTitle,
+        [String]$SheetName,
 
         [Parameter(Mandatory = $False)]
         [Int32]$MaxColumnWidth = 50,
@@ -66,25 +66,20 @@ Function Export-Excel {
             #-------- Add worksheet and format cells as text --------
             if ($newWorkbook) {
                 $workSheet = $WorkbookObject.Worksheets.item(1)
-            }
-            Else {
-                $workSheet = $WorkbookObject.Worksheets.Add()
-            }
-            $workSheet.Cells.NumberFormat = "@"
-            #------- Name the worksheet if given --------
-            If ($SheetName) {
-                $origSheetName = $SheetName
-                #-------- If sheetname exists sheet with next available index --------
-                For ($i=1; $i -lt 100; $i++) {
-                    Try {
-                        $workSheet.Name = $SheetName
-                        break
-                    }
-                    Catch {
-                        $SheetName = "$origSheetName$i"
-                    }
+                if ($SheetName) {
+                    $workSheet.name = $SheetName
                 }
             }
+            Else {
+                $WorkSheet = $WorkbookObject.Worksheets | Where-Object {$_.name -like $sheetName}
+                if (!$workSheet) {
+                    $workSheet = $WorkbookObject.Worksheets.add($sheetName)
+                }
+            }
+            #------- Set the worksheet cells format -------
+            $workSheet.Cells.NumberFormat = "@"
+            $range = $workSheet.Cells
+            $range.clear
         }
         Catch{
             If(!$WorkbookObject){
@@ -96,16 +91,14 @@ Function Export-Excel {
         }
     }
     Process {
-        # Set headers
+        #------- Set headers --------
         $headers = $PSOTable | Get-Member | Where-Object{$_.MemberType -eq "NoteProperty"}
         $headers | ForEach-Object{$workSheet.Cells.Item(1,($headers.indexof($_)) + 1) = $_.Name} 
-
-        # Format Worksheet Table
+        #-------- Format Worksheet Table ---------
         $ListObject = $workSheet.ListObjects.Add([Microsoft.Office.Interop.Excel.XlListObjectSourceType]::xlSrcRange, $WorkSheet.UsedRange, $null ,[Microsoft.Office.Interop.Excel.XlYesNoGuess]::xlYes)
         $listObject.Name = "Table1"
         $listObject.TableStyle = "TableStyleLight17"
-
-        # write collection to worksheet
+        #--------- write collection to worksheet -------
         $PSOTable | ForEach-Object{
             ForEach($header in $headers){$workSheet.Cells.Item(($PSOTable.indexof($_)) + 2,($headers.indexof($header)) + 1) = ($_."$($header.name)") -join "`n"}
         } 
@@ -128,13 +121,14 @@ Function Export-Excel {
         ForEach($column in $workSheet.UsedRange.EntireColumn){
             If($column.ColumnWidth -gt $MaxColumnWidth){$column.ColumnWidth = $MaxColumnWidth}
         }
-        $excel.visible = $True
-        # Cleanup
-        $x = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
-        Remove-Variable excel
-        If ($chart) {
-
-        }
+    }
+}
+end {
+    $excel.visible = $True
+    # Cleanup
+    $x = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
+    Remove-Variable excel
+    If ($chart) {
     }
 }    
     
